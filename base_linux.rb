d9998043fd68cc0d25bfa6c74774210a1c1fe407
Anthony Rees <anthony_rees01@hotmail.com>
2020-04-08 19:08:02 +1000

    Update base_linux.rb

--- base_linux.rb
+++ base_linux.rb
@@ -10,10 +10,13 @@ name 'base_linux'
 default_source :chef_server, "https://#{ENV['AUTOMATE_HOSTNAME']}/organizations/#{ENV['CHEF_ORG']}"
 
 # Specify a custom source for a cookbook:
-cookbook 'chef-client', '~> 10.2.2'
-# cookbook 'audit_agr', '~> 2.2.4' ## Stage 2
+cookbook 'chef-client', '~> 10.2.2' ## Stage 1 - Base
+# cookbook 'audit_agr', '~> 2.2.4' ## Stage 2 - Detect
+# cookbook 'cis-rhel', '~> 0.3.1'  ## Stage 3 - Correct
 
 # run_list: chef-client will run these recipes in the order specified.
 run_list 'chef-client' ## Stage 1
 
 # run_list 'chef-client', 'audit_agr'  ## Stage 2
+
+# run_list 'chef-client', 'audit_agr', 'cis-rhel'  ## Stage 3
