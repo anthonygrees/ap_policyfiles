0ff2310d93eed979d2240417e353e08598cbbc9c
Anthony Rees <anthony_rees01@hotmail.com>
2020-05-07 21:02:04 +1000

    Create policy_push.ps1

---
+++ policy_push.ps1
@@ -0,0 +1,6 @@
+chef update base_linux.rb
+chef update base_windows.rb
+chef update base_windows2012.rb
+chef push development base_linux.rb
+chef push development base_windows.rb
+chef push development base_windows2012.rb
