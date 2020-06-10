ccf71e952b7317df25003a3e5555bdfa81c1817c
Anthony Rees <anthony_rees01@hotmail.com>
2020-06-10 18:06:13 +1000

    Update policy_push.ps1

--- policy_push.ps1
+++ policy_push.ps1
@@ -1,6 +1,8 @@
 chef update base_linux.rb
 chef update base_windows.rb
 chef update base_windows2012.rb
+chef update linux_patch.rb
 chef push development base_linux.rb
 chef push development base_windows.rb
 chef push development base_windows2012.rb
+chef push development linux_patch.rb
