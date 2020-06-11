e92ed20ffe6a7396a116e6edc485c8c7d1f53694
Anthony Rees <anthony_rees01@hotmail.com>
2020-06-11 16:29:06 +1000

    Update policy_push.sh

--- policy_push.sh
+++ policy_push.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export AUTOMATE_HOSTNAME='anthony-a2.chef-demo.com'
+export CHEF_ORG='reesyorg'
+
 policies=('base_linux.rb' 'base_windows.rb' 'base_windows2012.rb' 'linux_patch.rb')
 
 for policy in ${policies[@]}; do
