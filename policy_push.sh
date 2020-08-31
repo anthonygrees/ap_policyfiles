7cfde713bdae42eaf424998fde3ea964ae83587e
Anthony Rees <anthony_rees01@hotmail.com>
2020-08-31 12:06:29 +1000

    Update policy_push.sh

--- policy_push.sh
+++ policy_push.sh
@@ -3,7 +3,7 @@
 export AUTOMATE_HOSTNAME='anthony-a2.chef-demo.com'
 export CHEF_ORG='reesyorg'
 
-policies=('base_linux.rb' 'base_windows.rb' 'base_windows2012.rb' 'linux_patch.rb')
+policies=('base_linux.rb' 'base_windows.rb' 'base_windows2012.rb' 'linux_patch.rb' 'patching.rb')
 
 for policy in ${policies[@]}; do
     chef install $policy
