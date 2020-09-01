cc8307202e51b47c08813a407f3d5a0d5e85b5a6
Anthony Rees <anthony_rees01@hotmail.com>
2020-09-02 08:12:12 +1000

    Create wsus.rb

---
+++ wsus.rb
@@ -0,0 +1,18 @@
+# base_windows.rb - Describe how you want Chef to build your system.
+#
+# For more information on the Policyfile feature, visit
+# https://github.com/opscode/chef-dk/blob/master/POLICYFILE_README.md
+# A name that describes what the system you're building with Chef does.
+
+name 'wsus'
+
+# Where to find external cookbooks:
+default_source :chef_server, "https://#{ENV['AUTOMATE_HOSTNAME']}/organizations/#{ENV['CHEF_ORG']}"
+
+# Specify a custom source for a cookbook:
+cookbook 'chef-client', '~> 12.2.0'  ## Stage 1 - Base
+cookbook 'audit_agr', '~> 2.2.4'   ## Stage 2 - Detect
+
+# run_list: chef-client will run these recipes in the order specified.
+
+run_list 'chef-client', 'audit_agr'  ## Stage 2 - Detect
