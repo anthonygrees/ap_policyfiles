1031542f7f3c606ed1c07f97fb123031406a0890
Anthony Rees <anthony_rees01@hotmail.com>
2020-09-16 15:31:29 +1000

    Update base_linux.rb

--- base_linux.rb
+++ base_linux.rb
@@ -15,8 +15,8 @@ cookbook 'audit_agr', '~> 2.2.4' ## Stage 2 - Detect
 cookbook 'cis-rhel', '~> 0.3.1'  ## Stage 3 - Correct
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list 'chef-client' ## Stage 1
+# run_list 'chef-client' ## Stage 1
 
-# run_list 'chef-client', 'audit_agr'  ## Stage 2
+run_list 'chef-client', 'audit_agr'  ## Stage 2
 
 # run_list 'chef-client', 'audit_agr', 'cis-rhel'  ## Stage 3
